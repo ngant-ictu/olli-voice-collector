f34f50c7a3dc205ff1a6f18819507f58612ed0be
Duy Olli <duy@olli-ai.com>
2018-09-27 15:42:39 +0700

    added tmp point

--- src/app/views/components/admin/user/point-record-item.vue
+++ src/app/views/components/admin/user/point-record-item.vue
@@ -5,6 +5,11 @@
       <i class="el-icon-fa-spinner el-icon-fa-spin" v-if="loading"></i>
       <strong v-else>{{ point }}</strong>
     </span>
+    <span class="tmp_point">
+      <small>TMP Point</small>:
+      <i class="el-icon-fa-spinner el-icon-fa-spin" v-if="loading"></i>
+      <strong v-else>{{ tmppoint }}</strong>
+    </span>
     <span class="record-times">
       <small>Record times</small>:
       <i class="el-icon-fa-spinner el-icon-fa-spin" v-if="loading"></i>
@@ -25,6 +30,7 @@ export default class PointRecordItem extends Vue {
   loading: boolean = false;
   point: number = 0;
   recordtimes: number = 0;
+  tmppoint: number = 0;
 
   async created() {
     this.loading = true;
@@ -32,6 +38,7 @@ export default class PointRecordItem extends Vue {
     return await this.getpointAction({ id: this.uid })
       .then(res => {
         this.point = res.data.point;
+        this.tmppoint = res.data.tmppoint;
         this.recordtimes = res.data.recordtimes;
 
         this.loading = false;
